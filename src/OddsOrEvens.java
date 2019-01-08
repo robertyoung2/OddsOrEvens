1892d8888baddc6c703699f920aa6eb34dcb9e5a
Robert Young <robert.young.ie@gmail.com>
2019-01-08 16:16:52 +0000

    All files add to repository

---
+++ src/OddsOrEvens.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class OddsOrEvens {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //INTRO
+        //take users name and if they are odds or evens
+
+        //RANDOMISER
+        //takes in the number of "fingers" the user will play
+        // and then uses random generator to give the computer a play value
+
+
+
+        //SUM
+        //adds the two integers together and produces the sum
+
+
+        //WHO WINS
+        //decides if final answers is odd or even, and declares the winner
+    }
+
+}
