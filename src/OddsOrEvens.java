db458153cfbec33e4c4061ca67c691f4a54d8fac
Robert Young <robert.young.ie@gmail.com>
2019-01-08 17:30:33 +0000

    Part 2 - "Play the Game" complete and functioning

--- src/OddsOrEvens.java
+++ src/OddsOrEvens.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class OddsOrEvens {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -6,27 +7,34 @@ public class OddsOrEvens {
         //INTRO
         //take users name and if they are odds or evens
 
-        System.out.println("Let's play a game called Odds and Evens");
+        System.out.println("Let's play a game called \"Odds and Evens\".");
         System.out.print("What is your name? ");
         String name = "";
         name += input.nextLine();
         System.out.print("Hi " + name + ", which do you choose? (O)dds or (E)vens? ");
         String choice = input.next();
+
         if (choice.equals("O") || choice.equals("o")) {
             System.out.println(name + " has picked odds! The computer will be evens.");
         }
         else {
             System.out.println(name + " has picked evens! The computer will be odds.");
         }
-
+        System.out.println("------------------------------");
+        System.out.println();
 
 
         //RANDOMISER
         //takes in the number of "fingers" the user will play
         // and then uses random generator to give the computer a play value
+
+        System.out.print("How many \"fingers\" do you put out? ");
+        int fingers = input.nextInt();
         Random random = new Random();
         int computer = random.nextInt(6);
-
+        System.out.println("The computer plays " + computer + " \"fingers\".");
+        System.out.println("------------------------------");
+        System.out.println();
 
         //SUM
         //adds the two integers together and produces the sum
